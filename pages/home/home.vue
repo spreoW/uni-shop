e61920165ccd60a4edcad17bb34e96808b69d618
quan <1563904374@qq.com>
2022-04-14 22:14:15 +0800

    轮播图的开发swiper

--- pages/home/home.vue
+++ pages/home/home.vue
@@ -1,6 +1,15 @@
 <template>
   <view>
-    Home
+    <!-- 轮播图区域 -->
+    <swiper :indicator-dots="true" :autoplay="true" :interval="3000" :duration="1000" :circular="true">
+      <!-- 循环渲染轮播图的 item 项 -->
+      <swiper-item v-for="(item, i) in swiperList" :key="i">
+        <view class="swiper-item">
+          <!-- 动态绑定图片的 src 属性 -->
+          <image :src="item.image_src"></image>
+        </view>
+      </swiper-item>
+    </swiper>
   </view>
 </template>
 
@@ -8,12 +17,44 @@
   export default {
     data() {
       return {
-        
-      };
-    }
+        // 1. 轮播图的数据列表，默认为空数组 
+        swiperList: [],
+      }
+    },
+    onLoad() {
+      // 2. 在小程序页面刚加载的时候，调用获取轮播图数据的方法 
+      this.getSwiperList()
+    },
+    methods: {
+      // 3. 获取轮播图数据的方法 
+      async getSwiperList() {
+        // 3.1 发起请求
+        const {
+          data: res
+        } = await uni.$http.get('/api/public/v1/home/swiperdata') 
+        // 3.2 请求失败
+        if (res.meta.status !== 200) {
+          return uni.showToast({
+            title: '数据请求失败!',
+            duration: 1500,
+            icon: 'none',
+          })
+        }
+        // 3.3 请求成功，为 data 中的数据赋值
+        this.swiperList = res.message
+      },
+    },
   }
 </script>
 
 <style lang="scss">
+  swiper {
+    height: 330rpx;
 
+    .swiper-item,
+    image {
+      width: 100%;
+      height: 100%;
+    }
+  }
 </style>
--- pages/my/my.vue
+++ pages/my/my.vue
@@ -1,6 +1,19 @@
 <template>
   <view>
-    My
+    <image src="../../static/my-icons/icon2.png"></image>
+    <swiper :indicator-dots="true" :autoplay="true" :interval="3000" :duration="1000" :circular="true">
+      <swiper-item>
+        <view class="swiper-item">
+          <!-- 动态绑定图片的 src 属性 -->
+          <image src="https://www.uinav.com/pyg/banner1.png"></image>
+        </view>
+      </swiper-item>
+      <swiper-item>
+        <view class="swiper-item">
+          <image src="../../static/tab_icons/cate-active.png"></image>
+        </view>
+      </swiper-item>
+    </swiper>
   </view>
 </template>
 
